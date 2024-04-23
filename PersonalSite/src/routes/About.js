b509190729c36742bb89db7865b3a13641f20046
edberisha <edberisha95@gmail.com>
2024-04-23 09:18:23 -0400

    changed directory folder name (removed hyphen)

---
+++ PersonalSite/src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Aboutcard from "../components/Aboutcard";
+
+const About = () => {
+  return (
+    <div>
+      <Navbar />
+      <Aboutcard />
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
