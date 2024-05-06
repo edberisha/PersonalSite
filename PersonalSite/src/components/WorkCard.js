fb2d217d0f10cf90e078e4ac67ce8944ed3a00e0
edberisha <edberisha95@gmail.com>
2024-05-06 16:14:35 -0400

    added peanut pals project

--- PersonalSite/src/components/WorkCard.js
+++ PersonalSite/src/components/WorkCard.js
@@ -5,27 +5,27 @@ import Potluck from "../assets/potluck.png";
 import Texas from "../assets/texas.png";
 import { NavLink } from "react-router-dom";
 import "./WorkCardStyles.css";
+import pp from "../assets/pp.png"
 
 const WorkCard = () => {
   return (
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
-        <div className="project-card">
-          <img src={MMORPG} alt="mmrpgphoto" />
+      <div className="project-card">
+          <img src={pp} alt="peakphoto" />
           <h2 className="project-title" style={{ color: "white" }}>
-            Math Mind RPG
+            Peanut Pals
           </h2>
           <div className="pro-content">
             <div className="pro-details">
               <p>
-                Math Mind RPG - Where math meets adventure! Test your
-                multiplication skills in this exciting role-playing game.
+                Peanut Pals. A mock service website offering mult-tier subscription options to premium services. Send us a message. Subscribe today. 
               </p>
             </div>
             <div className="pro-btns">
               <NavLink
-                to="https://github.com/edberisha/MathMindRPG"
+                to="https://edberisha.github.io/PeanutPals/"
                 className="btn"
               >
                 Go To Project
@@ -33,6 +33,7 @@ const WorkCard = () => {
             </div>
           </div>
         </div>
+       
         <div className="project-card">
           <img src={Peak} alt="peakphoto" />
           <h2 className="project-title" style={{ color: "white" }}>
@@ -89,6 +90,28 @@ const WorkCard = () => {
             <div className="pro-btns">
               <NavLink
                 to="https://edberisha.github.io/TexasHoldEm/"
+                className="btn"
+              >
+                Go To Project
+              </NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src={MMORPG} alt="mmrpgphoto" />
+          <h2 className="project-title" style={{ color: "white" }}>
+            Math Mind RPG
+          </h2>
+          <div className="pro-content">
+            <div className="pro-details">
+              <p>
+                Math Mind RPG - Where math meets adventure! Test your
+                multiplication skills in this exciting role-playing game.
+              </p>
+            </div>
+            <div className="pro-btns">
+              <NavLink
+                to="https://github.com/edberisha/MathMindRPG"
                 className="btn"
               >
                 Go To Project
